b635e837855b81b97500f73420672429c1e35583
That guy! <turkishshaman@gmail.com>
2016-02-11 23:00:42 -0700

    Create Debug Grid Exposure
    Attempt to find if it's possible to have the gadget stuff do the hard logic for us.

---
+++ Debug-Stuff/ExposeGrids.lua
@@ -0,0 +1,11 @@
+function widget:GetInfo()
+    return {
+      name      = "Testing_ExposeGridNumbers",
+      desc      = "expose the grid numbers",
+      author    = "Shaman",
+      date      = "2/11/16",
+      license   = "",
+      layer     = 5,
+      enabled   = true,
+    }
+  end
